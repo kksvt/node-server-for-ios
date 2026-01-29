dc43865de1ff09cc397a992405551b933f409a3a
Krzysztof <krzysztof.swiatek@student.uj.edu.pl>
2026-01-29 03:18:05 +0100

    auth fix

--- index.js
+++ index.js
@@ -99,6 +99,10 @@ app.use('/auth', (req, res, next) => {
             return res.status(403).json({message: 'Failed to authenticate'});
         }
         req.user = tmp_db.get(user.email);
+        if (req.user) {
+            //eh, this is possible
+            return res.status(403).json({message: 'Invalid user'});
+        }
         next();
     });
 });
